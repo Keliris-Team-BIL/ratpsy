7036284beb00ab291d69c5dd7444a27133b29053
Georgios A. Keliris <gkeliris@GKs-MacBookPro13.local>
2020-05-16 13:19:48 +0200

    added figures in Analysis folder

--- gks_psychometrics.m
+++ gks_psychometrics.m
@@ -4,6 +4,7 @@ data=gk_ratpsy_readData('/Users/gkeliris/GitHub/ratpsy/Data',forceRecalc);
 
 %% Collapse pre and post data for population analysis
 allDataPre=gk_ratpsy_collapseTimepoints(data,'allPre');
+linPost=gk_ratpsy_collapseTimepoints(data,{'post10','post11','post12'});
 logPost=gk_ratpsy_collapseTimepoints(data,{'post13','post14','post15'});
 
 %% plot the psychometric per rat
@@ -11,10 +12,12 @@ for rat=[7,8,11,19]
     display('_____________________________________________');
     fprintf('Processing rat: %d \n',rat);
     N_pre=gk_get_psychometric(allDataPre(allDataPre.ratNumber==rat,:));
-    N_post=gk_get_psychometric(logPost(logPost.ratNumber==rat,:));
+    N_post=gk_get_psychometric(linPost(linPost.ratNumber==rat,:));
+    N_postLog=gk_get_psychometric(logPost(logPost.ratNumber==rat,:));
     figure(rat); hold on;
     gk_PAL_PFML(N_pre,[1.0, 0.2, 0.2]);
     grid on;
     gk_PAL_PFML(N_post,[0.2, 0.2, 1.0]);
-    legend('pre','pre','post','post');
+    gk_PAL_PFML(N_postLog,[0.2, 1.0, 0.2]);
+    legend('pre','pre','post','post','postLog','postLog','Location','northwest');
 end
