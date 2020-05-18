bcbcd459008f310c34488576cedf552184964c31
Georgios A. Keliris <gkeliris@GKs-MacBookPro13.local>
2020-05-18 15:57:39 +0200

    added rat number in title

--- gks_psychometrics.m
+++ gks_psychometrics.m
@@ -15,9 +15,13 @@ for rat=[7,8,11,19]
     N_post=gk_get_psychometric(linPost(linPost.ratNumber==rat,:));
     N_postLog=gk_get_psychometric(logPost(logPost.ratNumber==rat,:));
     figure(rat); hold on;
-    gk_PAL_PFML(N_pre,[1.0, 0.2, 0.2]);
+    display('-------PRE---------');
+    gk_PAL_PFML(N_pre,[1.0, 0.2, 0.2],1);
     grid on;
-    gk_PAL_PFML(N_post,[0.2, 0.2, 1.0]);
-    gk_PAL_PFML(N_postLog,[0.2, 1.0, 0.2]);
+    display('-------POST--------');
+    gk_PAL_PFML(N_post,[0.2, 0.2, 1.0],1);
+    display('-------LOG---------');
+    gk_PAL_PFML(N_postLog,[0.2, 1.0, 0.2],1);
+    title(['Rat ',num2str(rat)]);
     legend('pre','pre','post','post','postLog','postLog','Location','northwest');
 end
