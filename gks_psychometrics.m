8d6b27986e777c008b5d3229a7a3366f18143659
Georgios A. Keliris <gkeliris@GKs-MacBookPro13.local>
2020-05-18 20:49:32 +0200

    corrected mistake in gks_psychometric

--- gks_psychometrics.m
+++ gks_psychometrics.m
@@ -11,17 +11,17 @@ logPost=gk_ratpsy_collapseTimepoints(data,{'post13','post14','post15'});
 for rat=[7,8,11,19]
     display('_____________________________________________');
     fprintf('Processing rat: %d \n',rat);
-    N_pre=gk_get_psychometric(allDataPre(allDataPre.ratNumber==rat,:));
-    N_post=gk_get_psychometric(linPost(linPost.ratNumber==rat,:));
-    N_postLog=gk_get_psychometric(logPost(logPost.ratNumber==rat,:));
+    N_pre=gk_get_psychometric(allDataPre(allDataPre.ratNumber==rat,:),1);
+    N_post=gk_get_psychometric(linPost(linPost.ratNumber==rat,:),1);
+    N_postLog=gk_get_psychometric(logPost(logPost.ratNumber==rat,:),1);
     figure(rat); hold on;
     display('-------PRE---------');
-    gk_PAL_PFML(N_pre,[1.0, 0.2, 0.2],1);
+    gk_PAL_PFML(N_pre,[1.0, 0.2, 0.2]);
     grid on;
     display('-------POST--------');
-    gk_PAL_PFML(N_post,[0.2, 0.2, 1.0],1);
+    gk_PAL_PFML(N_post,[0.2, 0.2, 1.0]);
     display('-------LOG---------');
-    gk_PAL_PFML(N_postLog,[0.2, 1.0, 0.2],1);
+    gk_PAL_PFML(N_postLog,[0.2, 1.0, 0.2]);
     title(['Rat ',num2str(rat)]);
     legend('pre','pre','post','post','postLog','postLog','Location','northwest');
 end
